4396c3ed4fb0b82982faa89b579771ded086193e
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-27 10:06:07 -0500

    loop for creating seat objects in the theater

--- TheaterProject/main.cpp
+++ TheaterProject/main.cpp
@@ -10,17 +10,35 @@
 #include "headers.h"
 #include "seats.cpp"
 
+
+
+
+
 int main(int argc, const char * argv[]) {
+//    define amount of rows and columns in the theater
     int chairs_in_row = 4;
     int row_of_chairs = 4;
-//    class Seat chairArray[row_of_chairs][chairs_in_row];
-std::cout.setf(std::ios::fixed | std::ios::showpoint | std::ios::left);
+    class Seat chairArray[row_of_chairs][chairs_in_row];
+    
     for(int row = 0; row < chairs_in_row; row++){
         std::cout << "row "<< row << " ";
         for(int column = 0; column < row_of_chairs; column++){
-            std::cout <<"#";
+            chairArray[row][column] = *new Seat;
+            chairArray[row][column].occupied = 1;
+        }
+    }
+//    loop to display whether seats are available or not
+    std::cout.setf(std::ios::fixed | std::ios::showpoint | std::ios::left);
+    for(int row = 0; row < chairs_in_row; row++){
+        std::cout << "row "<< row << " ";
+        for(int column = 0; column < row_of_chairs; column++){
+            if(chairArray[row][column].occupied  == 0){
+            std::cout << '#';
+            } else {
+            std::cout << '*';
             }
-        std::cout << std::endl;
+            }
+         std::cout << std::endl;
         }
     return 0;
 }
